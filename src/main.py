9ce157462992b0f26a17819990ee8fc24c818c03
AStudentProgrammer <linzhi263754@gmail.com>
2023-12-18 16:24:34 +0800

    Include in all 15 drones
    Along with up movements

--- src/main.py
+++ src/main.py
@@ -6,6 +6,19 @@ import time
 swarm = TelloSwarm.fromIps([
     "192.168.1.101",
     "192.168.1.102",
+    "192.168.1.103",
+    "192.168.1.104",
+    "192.168.1.105",
+    "192.168.1.106",
+    "192.168.1.107",
+    "192.168.1.108",
+    "192.168.1.109",
+    "192.168.1.110",
+    "192.168.1.111",
+    "192.168.1.112",
+    "192.168.1.113",
+    "192.168.1.114",
+    "192.168.1.115"
 ])
 
 # def battery_checker(drone_number, tello):
@@ -19,10 +32,19 @@ def right_movement(drone_number, tello):
     if drone_number > 1:
         tello.move_right(100)
 
+def forward_movement(drone_number, tello):
+    tello.move_forward(50)
+    swarm.sync()
+
+def up_movement(drone_number, tello):
+    tello.move_up(25)
+    swarm.sync()
+
 # main code
 swarm.connect()
-swarm.parallel(left_movement)
+swarm.parallel(up_movement)
 time.sleep(10)
-swarm.parallel(right_movement)
+swarm.parallel(up_movement)
+swarm.land()
 
 swarm.end()
