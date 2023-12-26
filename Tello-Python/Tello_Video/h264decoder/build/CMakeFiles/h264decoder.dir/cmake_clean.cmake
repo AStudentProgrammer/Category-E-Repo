77994293c884d462c512880c2160049123ce17d7
AStudentProgrammer <linzhi263754@gmail.com>
2023-12-26 15:36:51 +0800

    Using the camera of Tello
     - reedit cmakelist.txt to fit python3
    - change all instances of pystring to oybyte in h264decoder_python.cpp
    - change all Tkinter instaances to tkinter

---
+++ Tello-Python/Tello_Video/h264decoder/build/CMakeFiles/h264decoder.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h264decoder.dir/h264decoder.cpp.o"
+  "CMakeFiles/h264decoder.dir/h264decoder.cpp.o.d"
+  "CMakeFiles/h264decoder.dir/h264decoder_python.cpp.o"
+  "CMakeFiles/h264decoder.dir/h264decoder_python.cpp.o.d"
+  "libh264decoder.pdb"
+  "libh264decoder.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/h264decoder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Tello-Python/Tello_Video/h264decoder/build/CMakeFiles/h264decoder.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for h264decoder.
+# This may be replaced when dependencies are built.
