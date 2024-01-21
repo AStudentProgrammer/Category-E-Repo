69fc812a1608b3a7641ca4328ec220eb43bb2324
AStudentProgrammer <linzhi263754@gmail.com>
2024-01-21 19:29:10 +0800

    convert pixels to cm
    resize image to 500 pixels

--- src/main.py
+++ src/main.py
@@ -4,4 +4,12 @@ import json
 json_File = open("Plan 1", "r")
 sample_load_file = json.load(json_File)
 print(sample_load_file[0]["motion"])
-print(sample_load_file[0]["distance"])
+print(sample_load_file[0]["distance"]) # in pixels
+
+def pixels_To_cm(pixels):
+    
+    real_map = 2000 # 2000cm
+    picture_map = 500 # 500 pixels
+
+    cm = (real_map/picture_map) * pixels
+    return cm
