a1641008558c1d06d32fb17d3ae28ea314c3cc8b
AStudentProgrammer <linzhi263754@gmail.com>
2024-01-21 19:06:07 +0800

    Test "motion" and "distance"

--- src/main.py
+++ src/main.py
@@ -3,4 +3,5 @@ import json
 
 json_File = open("Plan 1", "r")
 sample_load_file = json.load(json_File)
-print(sample_load_file[0])
+print(sample_load_file[0]["motion"])
+print(sample_load_file[0]["distance"])
