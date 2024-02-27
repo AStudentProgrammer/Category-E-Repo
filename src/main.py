9cb60db0d41cfbeb0553fd36aeb8fb1627b5b9f4
AStudentProgrammer <linzhi263754@gmail.com>
2024-02-27 18:15:20 +0800

    working double video feed

--- src/main.py
+++ src/main.py
@@ -70,21 +70,32 @@ def waypoint_flight(drone_number, tello):
                 tello.send_keepalive()
                 m_id += 1
 
-tello_normal = Tello(host="192.168.1.105")
-# tello_normal_one = Tello()
+tello_normal = Tello(host="192.168.1.105", vs_udp=11111)
+tello_normal_six = Tello(host="192.168.1.106", vs_udp=11112)
+
 tello_normal.connect()
-# tello_normal_one.connect()
+tello_normal_six.connect()
+
+tello_normal.send_command_with_return("port 8890 11111")
+tello_normal_six.send_command_with_return("port 8890 11112")
 
 tello_normal.streamoff()
 tello_normal.streamon()
+
+tello_normal_six.streamoff()
+tello_normal_six.streamon()
+
 frame_read = tello_normal.get_frame_read()
+frame_read_six = tello_normal_six.get_frame_read()
 
 # tello.takeoff()
 
 while True:
 
     img = frame_read.frame
+    img_six = frame_read_six.frame
     cv2.imshow("drone", img)
+    cv2.imshow("drone 6 ", img_six)
 
     key = cv2.waitKey(1) & 0xff
     if key == 27: # ESC
